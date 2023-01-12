edb1cfce12446fd029089228232c69f03c8aad64
jpromanonet <programming@jpromano.net>
2023-01-12 10:59:02 -0300

    Ejercicio 01

--- Ejercicio_01/index.js
+++ Ejercicio_01/index.js
@@ -32,10 +32,52 @@ await mongoose.connect('mongodb://127.0.0.1/colegio', {
 console.log('Base de datos conectada');
 
 try {
+// Punto 1: Actualizar el DNI de Lucas Blanco
     console.log("Actualizar DNI de Lucas Blanco")
     let rta = await estudiantesDAO.updateOne({
         nombre: 'Lucas',
         apellido: 'Blanco'
     }, { $set: {dni: 20355875} })
     console.log(rta) 
+
+// Punto 2: Agregar un campo 'ingreso' a todos los documentos con el valor false
+    console.log("Agregar campo de ingreso a documentos con valor false")
+    // rta = await estudiantesDAO.updateMany({}, { ingreso: false })
+    rta = await estudiantesDAO.updateMany({}, { $set: { ingreso: false }})
+    console.log(rta)
+
+// Punto 3: Modificar el valor ingreso a true para todos los estudiantes del curso 1A
+    rta = await estudiantesDAO.updateMany({ curso: '1A'}, { $set: { ingreso: true} })
+    console.log(rta)
+
+// Punto 4: Listas los estudiantes que aprobaron (de 4 en adelante) sin los campos id y V
+    const estudiantesAprobados = await estudiantesDAO.find({ nota: { $gte: 4}}, {_id: 0, __v: 0})
+    estudiantesAprobados.forEach(estAprob => {
+        console.log(JSON.stringify(estAprob))
+    })
+
+// Punto 5: Listar los estudiantes que posean el campo 'ingreso' en true sin los de ID y V
+    const estudiantesIngresantes = await estudiantesDAO.find({ ingreso: true}, { _id: 0, __v: 0})
+    estudiantesIngresantes.forEach(estIngres => {
+        console.log(JSON.stringify(estIngres))
+    })
+
+// Punto 6: Borrar de la coleccion de estudiantes los que posean ingreso true
+    rta = await estudiantesDAO.deleteMany({ ingreso: true})
+    console.log(rta)
+
+// Punto 7: Listar el contenido de la colección estudiantes con el campo __v: 0
+    let estudiantes = await estudiantesDAO.find({}, {__v: 0})
+    estudiantes.forEach(estudiante => {
+        console.log(
+            JSON.stringify(estudiante),
+            '-> Fecha de creación: ',
+            new Date(estudiante._id.getTimestamp()).toLocaleString()
+        )
+    })
+
+} catch (err) {
+    console.log('Error en el proceso de base de datos', err)
+} finally {
+    await mongoose.disconnect()
 }
